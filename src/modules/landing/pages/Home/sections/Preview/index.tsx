0e7b4d1780f92827dc18968bfd773a502f8883e1
kalinichenko <ka.ki.ko.1995@gmail.com>
2022-06-24 12:00:50 +0300

    Fix Landing Preview layout

--- src/modules/landing/pages/Home/sections/Preview/index.tsx
+++ src/modules/landing/pages/Home/sections/Preview/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Button, Grid, Typography } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import { SocialLinks } from 'components';
 import { LogoBlackLittle } from 'components/Icon/components';
 import { FontFamilies } from 'theme/Typography';
@@ -7,43 +7,37 @@ import { COLOR_TEXT_BLUE } from 'theme/variables';
 
 export const Preview: FC = () => {
   return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="flex-start"
-      alignItems="flex-start"
-      spacing={3}
-      height={{ xs: '100vh', sm: '100vh', md: '70vh' }}
-      pt={{ xs: 20, sm: 20, md: 0 }}
-    >
-      <Grid item xs={12}>
-        <Typography variant="h1">Limitless, Borderless, All-in-One</Typography>
-        <Typography variant="h1" color={COLOR_TEXT_BLUE}>
-          Launch Zone
-        </Typography>
-        <Typography variant="h1">of the Metaverse</Typography>
-      </Grid>
+    <Box sx={{ pt: { xs: 20, sm: 20, md: 0 }, height: '100vh' }}>
+      <Grid container direction="column" justifyContent="flex-start" alignItems="flex-start" spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h1">Limitless, Borderless, All-in-One</Typography>
+          <Typography variant="h1" color={COLOR_TEXT_BLUE}>
+            Launch Zone
+          </Typography>
+          <Typography variant="h1">of the Metaverse</Typography>
+        </Grid>
 
-      <Grid item xs={12}>
-        <Typography
-          variant="h4"
-          sx={{
-            fontFamily: FontFamilies.primary,
-          }}
-        >
-          #1 Launch Zone on the First EVM and IBC interoperable Blockhain
-        </Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <SocialLinks />
-      </Grid>
-      <Grid item xs={12}>
-        <LogoBlackLittle />
-      </Grid>
+        <Grid item xs={12}>
+          <Typography
+            variant="h4"
+            sx={{
+              fontFamily: FontFamilies.primary,
+            }}
+          >
+            #1 Launch Zone on the First EVM and IBC interoperable Blockhain
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <SocialLinks />
+        </Grid>
+        <Grid item xs={12}>
+          <LogoBlackLittle />
+        </Grid>
 
-      <Grid item xs={12}>
-        <Button>Pitch Deck</Button>
+        <Grid item xs={12}>
+          <Button>Pitch Deck</Button>
+        </Grid>
       </Grid>
-    </Grid>
+    </Box>
   );
 };
